c7e4ca83158a36176ed791f4c37ce8e0da7cfe45
yaelBrown <yaelrbrown@gmail.com>
2022-05-14 18:49:41 -0500

    add: add migrate script

--- db/config/config.js
+++ db/config/config.js
@@ -1,10 +1,11 @@
 require("dotenv").config;
+
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "password",
+    database: process.env.DB_NAME || "kastle",
+    host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
   },
   test: {
